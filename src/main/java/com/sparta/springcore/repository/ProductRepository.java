b8356790875d949d2a2d22b3a4bc791b7638bf34
yeonu <reaqua07@naver.com>
2021-07-05 19:04:55 +0900

    4주차 5까지 내용

--- src/main/java/com/sparta/springcore/repository/ProductRepository.java
+++ src/main/java/com/sparta/springcore/repository/ProductRepository.java
@@ -1,10 +1,21 @@
 package com.sparta.springcore.repository;
 
 import com.sparta.springcore.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByUserId(Long userId);
+    Page<Product> findAllByUserId(Long userId, Pageable pageable);
+
+    // 2. 상품명이 title인 관심상품 1개 조회
+//    Product findByTitle(String title);
+
+    // 3. 상품명에 word가 포함된 모든 상품들 조회
+//    List<Product> findAllByTitleContaining(String word);
+
+    // 4. 최저가가 fromPrice ~ toPrice 인 모든 상품들을 조회
+//    List<Product> findAllByLpriceBetween(int fromPrice, int toPrice);
 }
--- src/main/java/com/sparta/springcore/service/ProductService.java
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -5,6 +5,10 @@ import com.sparta.springcore.dto.ProductRequestDto;
 import com.sparta.springcore.model.Product;
 import com.sparta.springcore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,6 +19,8 @@ import java.util.List;
 public class ProductService {
     // 멤버 변수 선언
     private final ProductRepository productRepository;
+    private static final int MIN_PRICE = 100;
+
 
     // 생성자: ProductService() 가 생성될 때 호출됨
     @Autowired              // IoC의 객체를 DI 받겠다
@@ -23,26 +29,37 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    // 회원 ID 로 등록된 모든 상품 조회
-    public List<Product> getProducts(Long userId) {
-        return productRepository.findAllByUserId(userId);
+
+    public Page<Product> getProducts(Long userId, int page, int size, String sortBy, boolean isAsc) {
+        // spring boot 제공
+        // 페이지 정렬
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return productRepository.findAllByUserId(userId, pageable);
     }
 
+
     @Transactional // 메소드 동작이 SQL 쿼리문임을 선언합니다.
-    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
+    public Product createProduct(ProductRequestDto requestDto, Long userId) {
         // 요청받은 DTO 로 DB에 저장할 객체 만들기
         Product product = new Product(requestDto, userId);
         productRepository.save(product);
         return product;
     }
 
+
     @Transactional
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         Product product = productRepository.findById(id).orElseThrow(
-                ()-> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
         );
-
+        // 변경될 관심 가격이 유효한지 확인합니다.
         int myPrice = requestDto.getMyprice();
+        if (myPrice < MIN_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_PRICE + " 원 이상으로 설정해 주세요.");
+        }
 
         // 기초 수업 버전
         product.updateMyPrice(myPrice);
@@ -54,7 +71,11 @@ public class ProductService {
     }
 
     // 모든 상품 조회 (관리자용)
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public Page<Product> getAllProducts(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return productRepository.findAll(pageable);
     }
+
 }
